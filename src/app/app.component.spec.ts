a972a089a65ab827ab2674c250a26f956e8e52f0
indiragundavarapu <gundavarapuindira9@gmail.com>
2025-06-17 14:05:00 -0700

    second commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
+  beforeEach(async() => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports:[AppComponent],
     }).compileComponents();
   });
 
